ec0eb8215fdc349b18c57f43e80bb6b53a2cee82
x54x6fx6d <98822656+x54x6fx6d@users.noreply.github.com>
2023-07-01 18:28:38 +0200

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -18,8 +18,8 @@
 
 
 std::string config::token() {
-    //return "OTk3NjQzNjgxNzAyMDg0NjMw.G4t5Tj.2bgtYbu4xn3kFy2fdjwQ-_IZowxNMMTkwmtzpk";
-    return "MTAxODIyNzY2NTI0MjY5NzgxOA.GfTQYD.DcTSQptmxsVBI_ptJbH8INUXLdiZXjfGt2GNt8";
+    std::string tk = "YOUR TOKEN";
+    return tk;
 }
 
 std::string config::prefix() {
